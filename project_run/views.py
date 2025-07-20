6d8c5be48af9b33443376b630e203cfe09b9e824
tata <tspan2017@gmail.com>
2025-07-20 22:05:29 +0200

    add company intro

---
+++ project_run/views.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def get_intro(request):    
+    company_dict = {
+        "company_name":settings.COMPANY_NAME,
+        "slogan":settings.SLOGAN,
+        "contacts":settings.CONTACTS
+    }    
+    return Response(company_dict)
+
+
