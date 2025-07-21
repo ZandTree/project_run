d012fb6e5967469e4786f6c35f8faec0abc80c9f
tata <tspan2017@gmail.com>
2025-07-21 13:32:29 +0200

    add model Run, serializer and viewset

---
+++ app_run/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import RunViewSetClass, get_intro
+
+router = routers.DefaultRouter()
+router.register(r'',RunViewSetClass)
+
+urlpatterns = [    
+    path('/company_details',get_intro),
+    path('/runs',include(router.urls)),
+    
+]
--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.conf import settings
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from app_run.models import Run
+
+from .serializers import RunSerializer
+
+
+@api_view(['GET'])
+def get_intro(request):    
+    company_dict = {
+        "company_name":settings.COMPANY_NAME,
+        "slogan":settings.SLOGAN,
+        "contacts":settings.CONTACTS
+    }    
+    return Response(company_dict)
+
+class RunViewSetClass(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
+    
