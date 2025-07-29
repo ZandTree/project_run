9bb100fb2cdf63152d2d53a318bb82e310f1c828
tata <tspan2017@gmail.com>
2025-07-29 23:05:39 +0200

    add new model Chellange

--- app_run/models.py
+++ app_run/models.py
@@ -1,16 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# class RunManager(models.Manager):
-#     def filter_superusers(self):
-#         return self.get_queryset().filter(athlete__user_is_superuser=False)
-    
+
 class Run(models.Model):
     class Status(models.TextChoices):
         INIT = "init", "Init"
-        IN_PROGRES = "in_progress", "Started"
+        IN_PROGRES = "in_progress", "In progress"
         FINISHED = "finished", "Finished"
-
     
     athlete = models.ForeignKey(User,on_delete=models.CASCADE,related_name="runs")
     created_at = models.DateTimeField(auto_now_add=True)
@@ -27,3 +23,10 @@ class AthleteInfo(models.Model):
     
     def __str__(self):
         return f"user id = {self.user.id}"
+    
+class Challenge(models.Model):
+    full_name = models.CharField(max_length=120)
+    athlete = models.ForeignKey(User,on_delete=models.CASCADE,related_name="challenges")   
+
+    def __str__(self):
+        return f"{self.full_name} for user {self.athlete}" 
