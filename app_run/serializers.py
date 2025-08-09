35b286d259181f561d8a9ed93244f28fb1641476
tata <tspan2017@gmail.com>
2025-08-09 20:29:09 +0200

    #16 fix new field in serializer

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -37,7 +37,7 @@ class RunSerializer(serializers.ModelSerializer):
     athlete_data = AthleteSerializer(source='athlete',read_only=True)    
     class Meta:
         model = Run        
-        fields = ["id","athlete","created_at","comment","athlete_data","status","distance"]
+        fields = ["id","athlete","created_at","comment","athlete_data","status","distance","run_time_seconds"]
 
 
 
