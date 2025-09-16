25f1ac457448a922d8f7c03ed87f26c0f937c9df
tata <tspan2017@gmail.com>
2025-09-16 22:06:22 +0200

    #20 return to only and distinct

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -111,12 +111,12 @@ class ChallengeSummarySerializer(serializers.Serializer):
         repr["name_to_display"] = instance.full_name
         repr["athletes"] = []   
         
-        users = User.objects.filter(is_staff=False).prefetch_related(
-        Prefetch("challenges", queryset=Challenge.objects.select_related("athlete"),to_attr="related_challenges")
-        ).only("first_name","last_name","username")       
         # users = User.objects.filter(is_staff=False).prefetch_related(
-        # Prefetch("challenges", queryset=Challenge.objects.select_related("athlete").only("full_name","athlete_id").distinct(),to_attr="related_challenges")
+        # Prefetch("challenges", queryset=Challenge.objects.select_related("athlete"),to_attr="related_challenges")
         # ).only("first_name","last_name","username")       
+        users = User.objects.filter(is_staff=False).prefetch_related(
+        Prefetch("challenges", queryset=Challenge.objects.select_related("athlete").only("full_name","athlete_id").distinct(),to_attr="related_challenges")
+        ).only("first_name","last_name","username")       
                                 
         for user in users:
             for challange in user.related_challenges:                
